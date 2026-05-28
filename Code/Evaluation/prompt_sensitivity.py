0bba98e25d63158ade3698e3d5d45435a11c5122
Sruthi Manoj <70858653+SruthiM-10@users.noreply.github.com>
2026-05-27 18:02:37 -0700

    Changed for .env file

--- Code/Evaluation/prompt_sensitivity.py
+++ Code/Evaluation/prompt_sensitivity.py
@@ -36,15 +36,8 @@ from pydantic import Field, conlist, BaseModel, ConfigDict
 from langchain_core.output_parsers import PydanticOutputParser
 from tqdm import tqdm
 tqdm.pandas()
-
-# Set environment variables for LangChain and OpenAI
-import os
-os.environ["LANGCHAIN_TRACING_V2"] = "false"
-os.environ["LANGCHAIN_API_KEY"] = ""
-os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGCHAIN_PROJECT"] = "irrigation-crawler"
-os.environ["OPENAI_API_KEY"] = ""
-# sm's acccount
+from dotenv import load_dotenv
+load_dotenv()
 
 OUTPUT_CSV_PATH = "/content/drive/MyDrive/1:1_Sruthi_Manoj/Dataset/csv_files (1-17-26)/Extra analysis/relevance_scoring_prompt_sensitivity.csv"
 
